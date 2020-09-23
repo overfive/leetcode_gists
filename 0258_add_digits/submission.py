a7ac3841cd01739f1e0d18c64e4378792bcaf9ba
corehello <corehello@gmail.com>
2020-09-23 23:20:22 +0800

    [0258] short logic

--- 0258_add_digits/submission.py
+++ 0258_add_digits/submission.py
@@ -1,6 +1,5 @@
 class Solution:
     def addDigits(self, num: int) -> int:
-        if num == 0:
-            return 0
-        else:
-            return (num - 1 ) % 9 + 1
+        if num % 9 == 0:
+            return 9
+        return num % 9
